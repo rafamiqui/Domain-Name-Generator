ab81cc8baf8e8029a69d79129c469ef6d006ea20
rafamiqui <miquilarenarafael18@gmail.com>
2022-08-03 01:08:43 +0000

    My first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+ for(let i = 0; i < pronoun.length; i++){
+      for(let k = 0; k < adj.length; k++){
+         for(let m = 0; m < noun.length; m++){
+            console.log(pronoun[i] + adj[k] + noun[m] + ".com")
+         }
+
+    }  
+    
+ }
